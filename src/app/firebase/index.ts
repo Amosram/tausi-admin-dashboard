4e817bc5dcf3a32159ba159047696fc69b660e5a
Timothy Mugo Gachengo <timomugo01@gmail.com>
2024-08-15 14:21:00 +0300

    add firebase configuration

---
+++ src/app/firebase/index.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCveGQtLfYySfu1YP0v5cvWxmDsReB3JqA",
+    authDomain: "tausi-app.firebaseapp.com",
+    projectId: "tausi-app",
+    storageBucket: "tausi-app.appspot.com",
+    messagingSenderId: "164460393814",
+    appId: "1:164460393814:web:ceca91decb00405fe0d2f7",
+    measurementId: "G-S1BNRKZP4K"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+
