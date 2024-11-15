cdbdb55e3ce07931bab723ddb667ef7d5dff5e94
samuelsenerwa <samuelsenerwa@gmail.com>
2024-11-15 16:45:52 +0300

    TAD-6 chore: added revenue folder structure

--- src/modules/revenue/pages/Revenue.tsx
+++ src/modules/revenue/pages/Revenue.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 const Revenue = () => {
   return (
     <div>Revenue</div>
-  )
-}
+  );
+};
 
-export default Revenue
+export default Revenue;
